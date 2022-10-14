d9eb29775b46e09e4b02ce5efb1551882ee977ce
tsubasayoshida <k021g1136@g.neec.ac.jp>
2022-10-14 15:56:34 +0900

    02_01.テクスチャと法線

--- Object3d.cpp
+++ Object3d.cpp
@@ -405,7 +405,8 @@ void Object3d::CreateModel()
 	//ファイルストリーム
 	std::ifstream file;
 	//.objファイルを開く
-	file.open("Resources/triangle.obj");
+	//file.open("Resources/triangle.obj");
+	file.open("Resources/triangle_tex.obj");
 	//ファイルオープン失敗をチェック
 	assert(!file.fail());
 
@@ -433,9 +434,37 @@ void Object3d::CreateModel()
 			//座標データに追加
 			positions.emplace_back(position);
 			//頂点データに追加
-			VertexPosNormalUv vertex{};
+			/*VertexPosNormalUv vertex{};
 			vertex.pos = position;
-			vertices.emplace_back(vertex);
+			vertices.emplace_back(vertex);*/
+		}
+
+		
+		//先頭文字列がvtならテクスチャ
+		if (key == "vt")
+		{
+			//U,V成分読み込み
+			XMFLOAT2 texcoord{};
+			line_stream >> texcoord.x;
+			line_stream >> texcoord.y;
+			//V方向反転
+			texcoord.y = 1.0f - texcoord.y;
+			//テクスチャ座標データに追加
+			texcoords.emplace_back(texcoord);
+
+		}
+
+		//先頭文字列がvnなら法線ベクトル
+		if (key == "vn")
+		{
+			//X,Y,Z成分読み込み
+			XMFLOAT3 normal{};
+			line_stream >> normal.x;
+			line_stream >> normal.y;
+			line_stream >> normal.z;
+			//法線ベクトルデータに追加
+			normals.emplace_back(normal);
+
 		}
 
 		//先頭文字列がfならポリゴン(三角形)
@@ -447,10 +476,22 @@ void Object3d::CreateModel()
 			{
 				//頂点インデックス1個分の文字列をストリームに変換して解析しやすく
 				std::istringstream index_stream(index_string);
-				unsigned short indexPosition;
+				unsigned short indexPosition, indexNormal, indexTexcoord;
 				index_stream >> indexPosition;
+				index_stream.seekg(1, ios_base::cur);  //スラッシュを飛ばす
+				index_stream >> indexTexcoord;
+				index_stream.seekg(1, ios_base::cur);  //スラッシュを飛ばす
+				index_stream >> indexNormal;
+				//頂点データの追加
+				VertexPosNormalUv vertex{};
+				vertex.pos = positions[indexPosition - 1];
+				vertex.normal = normals[indexNormal - 1];
+				vertex.uv = texcoords[indexTexcoord - 1];
+				vertices.emplace_back(vertex);
+				//インデックスデータの追加
+				indices.emplace_back((unsigned short)indices.size());
 				//頂点インデックスに追加
-				indices.emplace_back(indexPosition - 1);
+			    //indices.emplace_back(indexPosition - 1);
 			}
 		}
 
